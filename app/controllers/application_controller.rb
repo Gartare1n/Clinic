b1e9ae3bd1e465e6842f1198c296e9cfee4188c8
Mono <snayper00001@gmail.com>
2022-06-08 15:09:17 +0300

    Main functionality

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,13 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  
+  rescue_from CanCan::AccessDenied do
+    flash[:error] = 'Access denied!'
+    redirect_to root_url
+  end
+
+  def current_ability
+    @current_ability ||= ::Ability.new((doctor_signed_in?) ? current_doctor : current_patient)
+  end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  devise_for :doctors, path: 'doctors', controllers: { sessions: "doctors/sessions", registrations: "doctors/registrations"}
+  devise_for :patients, path: 'patients', controllers: { sessions: "patients/sessions", registrations: "patients/registrations"}
+  
+  root "welcome#index"
+
+  resources :appointments
+  resources :patients, only: [:index, :show]
+  resources :doctors, only: [:index, :show, :profile]
+  root :to => 'appointments#index', :constraints => lambda { |request| request.env['warden'].user.class.name == 'Doctor' }, :as => "doctor_root"
+  root :to => 'appointments#index', :constraints => lambda { |request| request.env['warden'].user.class.name == 'Patient' }, :as => "patient_root"
+
+end
